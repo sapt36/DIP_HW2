ee8554a65692eda2a07fbc9f4a8ca6b3a710c8e2
AndrewChen <853ujm@gmail.com>
2024-09-25 12:59:07 +0800

    style. add comment to all function

--- envForExe/HW2.py
+++ envForExe/HW2.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 class ImageProcessingApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('圖像處理程式')
+        self.setWindowTitle('HW2 圖像處理軟體')
         self.image = None
         self.initUI()
         self.setGeometry(100, 100, 1200, 900)  # 放大視窗尺寸
@@ -37,47 +37,48 @@ class ImageProcessingApp(QWidget):
         layout.addWidget(self.scroll_area)
 
         # 載入圖片按鈕
-        load_button = QPushButton('載入圖片', self)
+        load_button = QPushButton('1. 載入並顯示彩色 BMP 或 JPEG圖片', self)
         load_button.clicked.connect(self.load_image)
         layout.addWidget(load_button)
 
         # 灰階處理按鈕
-        gray_button = QPushButton('轉換為灰階並比較差異', self)
+        gray_button = QPushButton('2. 使用兩種公式將圖片轉換為灰階 並使用圖像相減做比較結果顯示', self)
         gray_button.clicked.connect(self.convert_to_grayscale_and_compare)
         layout.addWidget(gray_button)
 
-        # 調整亮度與對比按鈕
-        adjust_button = QPushButton('調整亮度與對比', self)
-        adjust_button.clicked.connect(self.open_brightness_contrast_dialog)
-        layout.addWidget(adjust_button)
+        # 直方圖顯示按鈕
+        hist_button = QPushButton('3. 顯示灰階直方圖', self)
+        hist_button.clicked.connect(self.show_histogram)
+        layout.addWidget(hist_button)
+
+        # 手動二值化按鈕
+        thresh_button = QPushButton('4. 將灰階圖像轉換為二值化圖像', self)
+        thresh_button.clicked.connect(self.manual_threshold)
+        layout.addWidget(thresh_button)
 
         # 調整空間解析度按鈕
-        resize_button = QPushButton('調整空間解析度', self)
+        resize_button = QPushButton('5-1. 調整空間解析度（放大或縮小）', self)
         resize_button.clicked.connect(self.open_resolution_dialog)
         layout.addWidget(resize_button)
 
         # 調整灰階級別按鈕
-        grayscale_levels_button = QPushButton('調整灰階級別', self)
+        grayscale_levels_button = QPushButton('5-2. 調整灰階級別', self)
         grayscale_levels_button.clicked.connect(self.open_grayscale_dialog)
         layout.addWidget(grayscale_levels_button)
 
-        # 手動二值化按鈕
-        thresh_button = QPushButton('手動二值化圖片', self)
-        thresh_button.clicked.connect(self.manual_threshold)
-        layout.addWidget(thresh_button)
+        # 調整亮度與對比按鈕
+        adjust_button = QPushButton('6. 調整亮度與對比度', self)
+        adjust_button.clicked.connect(self.open_brightness_contrast_dialog)
+        layout.addWidget(adjust_button)
 
-        # 直方圖顯示按鈕
-        hist_button = QPushButton('顯示灰階直方圖', self)
-        hist_button.clicked.connect(self.show_histogram)
-        layout.addWidget(hist_button)
-
-        # 直方圖均衡化保持顏色按鈕
-        hist_equalize_button = QPushButton('直方圖均衡化', self)
+        # 直方圖均衡化按鈕
+        hist_equalize_button = QPushButton('7. 自動對比度調整(直方圖均衡化)', self)
         hist_equalize_button.clicked.connect(self.histogram_equalization_color)
         layout.addWidget(hist_equalize_button)
 
         self.setLayout(layout)
 
+    # 載入圖片，並顯示在主視窗上
     def load_image(self):
         file_name, _ = QFileDialog.getOpenFileName(self, '選擇圖片', '', 'Image files (*.jpg *.jpeg *.bmp)')
         if file_name:
@@ -87,8 +88,8 @@ class ImageProcessingApp(QWidget):
             else:
                 self.display_image(self.image)
 
+    # 將 OpenCV 圖片轉換為 QImage 並顯示，保持原始大小
     def display_image(self, img):
-        """ 將 OpenCV 圖片轉換為 QImage 並顯示，保持原始大小 """
         qformat = QImage.Format_RGB888 if len(img.shape) == 3 else QImage.Format_Grayscale8
         h, w = img.shape[:2]
         img = QImage(img.data, w, h, img.strides[0], qformat)
@@ -99,6 +100,7 @@ class ImageProcessingApp(QWidget):
         self.image_label.setPixmap(pixmap)
         self.image_label.adjustSize()  # 調整 QLabel 大小以適應圖片原始尺寸
 
+    # 轉換圖片為灰階並比較使用兩種公式生成的灰階圖像
     def convert_to_grayscale_and_compare(self):
         if self.image is not None:
             # 獲取圖像的高、寬以及 RGB 通道數
@@ -145,8 +147,8 @@ class ImageProcessingApp(QWidget):
             difference = cv2.absdiff(gray_avg, gray_weighted)
             self.display_image(difference)
 
+    # 將 OpenCV 圖像轉換為 QPixmap
     def convert_cv_to_pixmap(self, cv_img):
-        """ 將 OpenCV 圖像轉換為 QPixmap """
         qformat = QImage.Format_RGB888 if len(cv_img.shape) == 3 else QImage.Format_Grayscale8
         h, w = cv_img.shape[:2]
         img = QImage(cv_img.data, w, h, cv_img.strides[0], qformat)
@@ -155,6 +157,7 @@ class ImageProcessingApp(QWidget):
         pixmap = QPixmap.fromImage(img)
         return pixmap
 
+    # 彈出調整亮度與對比的對話框
     def open_brightness_contrast_dialog(self):
         if self.image is not None:
             dialog = BrightnessContrastDialog(self)
@@ -162,17 +165,20 @@ class ImageProcessingApp(QWidget):
                 brightness, contrast = dialog.get_values()
                 self.adjust_brightness_contrast(brightness, contrast)
 
+    # 調整圖片的亮度與對比度
     def adjust_brightness_contrast(self, brightness, contrast):
         if self.image is not None:
             adjusted_img = cv2.convertScaleAbs(self.image, alpha=contrast, beta=brightness)
             self.display_image(adjusted_img)
 
+    # 將灰階圖像轉換為二值化圖像
     def manual_threshold(self):
         if self.image is not None:
             gray_img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
             _, binary_img = cv2.threshold(gray_img, 128, 255, cv2.THRESH_BINARY)
             self.display_image(binary_img)
 
+    # 計算並顯示灰階圖像對應之直方圖
     def show_histogram(self):
         if self.image is not None:
             gray_img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
@@ -221,7 +227,7 @@ class ImageProcessingApp(QWidget):
             equalized_img = cv2.cvtColor(yuv_img, cv2.COLOR_YUV2BGR)
             self.display_image(equalized_img)
 
-
+# 創建一個對話框(彈出視窗)，使用者可以輸入亮度和對比度的數值調整圖片，並點擊確認或取消按鈕。
 class BrightnessContrastDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -262,11 +268,11 @@ class BrightnessContrastDialog(QDialog):
             return None, None
 
 
-# ResolutionDialog類，彈出輸入框
+# 創建一個對話框，可輸入參數調整放大或縮小倍數 (0.1 - 5.0)
 class ResolutionDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle('調整空間解析度')
+        self.setWindowTitle('調整圖片大小')
         self.scale_input = QLineEdit(self)
         self.scale_input.setPlaceholderText('放大或縮小倍數 (0.1 - 5.0)')
 
@@ -293,7 +299,7 @@ class ResolutionDialog(QDialog):
             return None
 
 
-# GrayscaleDialog類，彈出輸入框
+# 創建一個對話框，可輸入參數調整灰階級別(2 - 256)
 class GrayscaleDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -322,8 +328,6 @@ class GrayscaleDialog(QDialog):
         except ValueError as e:
             QMessageBox.warning(self, '輸入錯誤', str(e))
             return None
-
-
 
 
 if __name__ == '__main__':
